2b220a40e8d88d368cc7f5ecd18004e7151443d8
PsiRamosFBC <thedigitaldoctor15@gmail.com>
2022-09-18 22:05:16 -0300

    setup VRF

--- contracts/FrogxVerse.sol
+++ contracts/FrogxVerse.sol
@@ -7,25 +7,32 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 contract FrogxVerse is ERC721, VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface COORDINATOR;
-    address vrfCoordinator;
-    bytes32 keyHash;
-    uint256 callBackGasLimit;
-    uint16 requestConfirmations;
-    uint32 numWords;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    event RequestedMint(uint256 indexed requestId)
 
     constructor(
         address _VRFCoordinator,
-        bytes32 _keyHash
+        bytes32 _gasLane,
+        uint64 _subscriptionId,
+        uint32 _callbackGasLimit
     )
     ERC721("FrogxVerse", "FROGXEL")
     VRFConsumerBaseV2(_VRFCoordinator){
-        vrfCoordinator = _VRFCoordinator;
-
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_VRFCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit
     }
 
     function mintFrogxel(address to, uint256 tokenId) external {
-        
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS);
+        emit RequestedMint(requestId)
     }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {

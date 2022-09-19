76ac816ca4c747f0c9207703e0dc37a9469f2c5d
PsiRamosFBC <thedigitaldoctor15@gmail.com>
2022-09-18 21:25:54 -0300

    add basic vrfV2

--- contracts/FrogxVerse.sol
+++ contracts/FrogxVerse.sol
@@ -4,12 +4,31 @@ pragma solidity >=0.8.10 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract FrogxVerse is ERC721 {
-    constructor()ERC721("FrogxVerse", "FROGXEL"){
+contract FrogxVerse is ERC721, VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+    address vrfCoordinator;
+    bytes32 keyHash;
+    uint256 callBackGasLimit;
+    uint16 requestConfirmations;
+    uint32 numWords;
+
+    constructor(
+        address _VRFCoordinator,
+        bytes32 _keyHash
+    )
+    ERC721("FrogxVerse", "FROGXEL")
+    VRFConsumerBaseV2(_VRFCoordinator){
+        vrfCoordinator = _VRFCoordinator;
+
     }
 
-    function mintFrogxel(address to, uint256 tokenId) public {
-        _safeMint(to, tokenId);
+    function mintFrogxel(address to, uint256 tokenId) external {
+        
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+
     }
 }
